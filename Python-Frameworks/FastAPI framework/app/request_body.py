70e26bded4d54103406a2fc74a04839c722c174d
Gaurav Panwar <gauravpanwar731@gmail.com>
2026-05-20 22:18:02 +0530

    modified request model

--- Python-Frameworks/FastAPI framework/app/request_body.py
+++ Python-Frameworks/FastAPI framework/app/request_body.py
@@ -1,3 +1,8 @@
+"""
+A request body is the data sent by a client (like a browser or a mobile app) to a server as part of an API request. 
+It typically contains information that the server needs to process the request, such as user input, form data, or JSON payloads.
+"""
+
 from fastapi import FastAPI
 
 app = FastAPI()
