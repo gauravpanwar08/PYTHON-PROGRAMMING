3d866c54876277e1d7a0b9310f04bf76cd67726d
Gaurav Panwar <gauravpanwar731@gmail.com>
2026-05-24 23:37:49 +0530

    FastAPI introduction endpoint

--- Python-Frameworks/FastAPI framework/main.py
+++ Python-Frameworks/FastAPI framework/main.py
@@ -1,6 +1,13 @@
+# INTRODUCTION TO FASTAPI:
+# FastAPI is a modern, fast (high-performance) web framework for building APIs with Python 3.6+ based on standard Python type hints.
+# ----------------------------------------------------------------------------------------------------------------------------------
+
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="FastAPI Concepts",
+    description="A collection of FastAPI concepts and examples"
+)
 
 @app.get("/")
 def home():
